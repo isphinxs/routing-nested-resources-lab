6273f92928854789395eb54b593af059ffa29dc6
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-13 22:23:03 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  resources :artists, except: [:show]
+  
+  resources :artists, only: [:show] do
+    resources :songs, only: [:show, :index]
+  end
+  
+  # still want users to be able to see all songs, create and edit songs, etc.
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 end
